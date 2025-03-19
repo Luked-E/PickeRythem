88e4ab6fd29787e9d5d59bd85e4c8aa12211d2ee
Lucas Erickson <148932045+Luked-E@users.noreply.github.com>
2025-03-19 15:11:30 -0700

    Update code.py
    Added LED debugging

--- code.py
+++ code.py
@@ -8,6 +8,9 @@ from digitalio import DigitalInOut, Direction, Pull # type: ignore
 #CT is current time
 from time import monotonic as CT  #CT is current time
 
+led = DigitalInOut(board.LED)
+led.direction = Direction.OUTPUT
+
 # Button inputs 1-4
 in1 = DigitalInOut(board.GP0)
 in1.direction = Direction.INPUT
@@ -30,8 +33,11 @@ ryth1 = DigitalInOut(board.GP5)
 ryth1.direction = Direction.OUTPUT
 ryth1.value = True
 
-false1= DigitalInOut(board.GP6)
+false1= DigitalInOut(board.GP4)
 false1.direction = Direction.OUTPUT
+
+false2= DigitalInOut(board.GP6)
+false2.direction = Direction.OUTPUT
 
 correct1= DigitalInOut(board.GP1)
 correct1.direction = Direction.OUTPUT
@@ -42,7 +48,7 @@ correct2.direction = Direction.OUTPUT
 #controls colored LED 1
 color1 = pwmio.PWMOut(board.GP16, duty_cycle= 64554)
 color2 = pwmio.PWMOut(board.GP17, duty_cycle= 64554)
-color3 =  pwmio.PWMOut(board.GP18, duty_cycle= 64554)
+color3 =  pwmio.PWMOut(board.GP18, duty_cycle= 64555)
 
 #game general
 difficulty=1
@@ -67,15 +73,23 @@ homeRythem = 1
 #Methods
 def GetInputs():
     global inputs
-    inputs=[not in2.value]#,not in3.value]
+    inputs=[not in2.value,not in3.value]
 
-def correctLED(state,light):
+def CorrectLED(state,light):
     global correct1
     global correct2
     if (light==0):
         correct1.value = state
     if (light==1):
         correct2.value = state
+
+def FalseLED(state,light):
+    global false1
+    global false2
+    if (light==0):
+        false1.value = state
+    if (light==1):
+        false2.value = state
 
 def ResetInputs():
     ct = CT()
@@ -115,10 +129,15 @@ ResetInputs()
 #run time
 while(True):
 
-    #while(inMenu)
-
+    while(inMenu):
+        if(not in4.value):
+            break
+    print("get ready")
+    time.sleep(2)
+    print("out")
     while(inGame):
-
+        if(not in1.value):
+            break
 
         if(Timer2(homeRythem, lastBeat)):
             lastBeat = CT()
@@ -128,21 +147,24 @@ while(True):
         GetInputs()
         currentInput=0
         for input in inputs:
-            #print(str(currentInput)+"---------")
 
+            #rests 
             if ((successNotReset[currentInput]) and (Timer2(rythem[currentInput],lastBeats[currentInput]-(timeRange/2)))):
                 success[currentInput] = False
                 successNotReset[currentInput] = False
-                false1.value=False
-                correctLED(False,currentInput)
+                FalseLED(False,currentInput)
+                CorrectLED(False,currentInput)
                 print("success: reset")
-            if Timer2(rythem[currentInput],lastBeats[currentInput]):
+
+                
+            if Timer2(rythem[currentInput],lastBeats[currentInput]):#resets the last beat 
                 successNotReset[currentInput] = True
                 lastBeats[currentInput] = CT()
                 '''if not success[currentInput]:
                     strikes += 1'''
                 print("lastBeat: reset, Strikes: "+str(strikes) )
             
+            led.value = input
             if (input):
                 if (not alreadyTrackedInput[currentInput]):
                     print("(")
@@ -150,16 +172,16 @@ while(True):
                     #adds strikes for pressing after a succes and pressing outside of acceptable zone
                     if (not Timer3(rythem[currentInput],lastBeats[currentInput],timeRange)):
                         strikes += 1
-                        false1.value=True
+                        FalseLED(True,currentInput)
                         print("Strikes   __1__   :"+str(strikes))
                     elif (success[currentInput]):
                         strikes += 1
-                        false1.value=True
+                        FalseLED(True,currentInput)
                         print("Strikes   __2__   :"+str(strikes))
                     else:
                         success[currentInput] = True
                         score += 1
-                        correctLED(True,currentInput)
+                        CorrectLED(True,currentInput)
                         print("Score: "+str(score))
 
             else:   # One registered press per on/off cycle
@@ -169,7 +191,7 @@ while(True):
             currentInput += 1
 
 
-        if Timer3Offset(homeRythem,lastBeat,0.1,-timeRange/2):# Updating rythem light
+        if Timer3Offset(homeRythem,lastBeat,0.1,-timeRange/4):# Updating rythem light
             ryth1.value = True
         else:
             ryth1.value = False
