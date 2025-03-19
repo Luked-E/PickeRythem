f05a15ead9ffdac1ad2be6e201c6aa1d0de92d05
Lucas Erickson <148932045+Luked-E@users.noreply.github.com>
2025-03-19 12:39:34 -0700

    Additions to code base.
    Added scoring and penalty system and added timed correlated lights.
    Current major errors are:
    repeated input detection without human input,

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # PickeRythem
  A small game made for the Raspberry Pi Pico using CircuitPython, a MicroPython derivative.
+
+# Code Base:
+### timing:
+Inputs are checked with a range function that resets half way through a successful range. So the Check input function("Timer3()" method) allows for scoring in half of the acceptable input range at the beginning and that range at the start.
--- code.py
+++ code.py
@@ -13,15 +13,15 @@ in1 = DigitalInOut(board.GP0)
 in1.direction = Direction.INPUT
 in1.pull = Pull.UP
 
-in2 = DigitalInOut(board.GP1)
+in2 = DigitalInOut(board.GP3)
 in2.direction = Direction.INPUT
 in2.pull = Pull.UP
 
-in3 = DigitalInOut(board.GP2)
+in3 = DigitalInOut(board.GP8)
 in3.direction = Direction.INPUT
 in3.pull = Pull.UP
 
-in4 = DigitalInOut(board.GP3)
+in4 = DigitalInOut(board.GP11)
 in4.direction = Direction.INPUT
 in4.pull = Pull.UP
 
@@ -30,6 +30,15 @@ ryth1 = DigitalInOut(board.GP5)
 ryth1.direction = Direction.OUTPUT
 ryth1.value = True
 
+false1= DigitalInOut(board.GP6)
+false1.direction = Direction.OUTPUT
+
+correct1= DigitalInOut(board.GP1)
+correct1.direction = Direction.OUTPUT
+
+correct2= DigitalInOut(board.GP10)
+correct2.direction = Direction.OUTPUT
+
 #controls colored LED 1
 color1 = pwmio.PWMOut(board.GP16, duty_cycle= 64554)
 color2 = pwmio.PWMOut(board.GP17, duty_cycle= 64554)
@@ -37,8 +46,11 @@ color3 =  pwmio.PWMOut(board.GP18, duty_cycle= 64554)
 
 #game general
 difficulty=1
-timeRange = 0.05
-gameEnd = False
+timeRange = 0.3
+strikes=0
+score=0
+inGame = True
+inMenu = True
 
 #input list
 inputs=[]
@@ -49,72 +61,118 @@ lastBeat = time.monotonic()
 rythemLightDuration = 0.25
 pausestart = 0
 pause_duration = 4
+homeRythem = 1
+
 
 #Methods
+def GetInputs():
+    global inputs
+    inputs=[not in2.value]#,not in3.value]
+
+def correctLED(state,light):
+    global correct1
+    global correct2
+    if (light==0):
+        correct1.value = state
+    if (light==1):
+        correct2.value = state
+
 def ResetInputs():
-    global NumInputs
-    #sets inputs function and rests variables
-    if NumInputs==2:
-        def NumInputs():
-            global inputs
-            inputs=[in2.value,in3.value]
-        global lastbeats
-        lastbeats = [CT,CT]
-        global rythem
-        rythem = [1,1]
-        global success
-        success = [False,False]
-        global alreadyTrackedInput
-        alreadyTrackedInput = [False,False]
-
-    else:
-        def NumInputs():
-            global inputs
-            inputs=[in2.value]
-        global rythem
-        rythem = [1]
-        global lastbeats
-        lastbeats = [CT]
-        global success
-        success = [False]
-        global alreadyTrackedInput
-        alreadyTrackedInput = [False]
+    ct = CT()
+    global lastBeats
+    lastBeats = [ct,ct]
+    global rythem
+    rythem = [1,1]
+    global alreadyTrackedInput
+    alreadyTrackedInput = [False,False]
+    global success
+    success = [0,0]
+    global successNotReset
+    successNotReset = [True,True]
 
 
-
-def Timer2(timing, lastBeat):
+def Timer2(timing, lastBeat): #gives if the range of lastbeat and currentbeat is greater than the timing
     fullBeat = (CT()-lastBeat)>=timing
-    return fullBeat # returns boolean
+    return fullBeat
 
 def Timer3(timing, lastBeat, range):
-    fullBeat = ((CT()-lastBeat) >= (timing-range)) and ((CT()-lastBeat) <= (timing+range))
-    return fullBeat # returns boolean
+    '''each compared to time past: ((above -range+) and (under +range+time)) or ((above zero) 
+    and (under range)) '''
+    fullBeat = (((CT()-lastBeat)>=(timing-(range/2))) and ((CT()-lastBeat)<=(timing+(range/2)))) or (((CT()-lastBeat)<=range/2) and ((CT()-lastBeat)>=0))
+    return fullBeat
 
+def Timer3Offset(timing, lastBeat, range, offset):
+    '''((above -range+) and (under +range+time)) or ((above zero) 
+    and (under range)) with offset making the base time shorter
+    offset does not work for offsets larger than half the range'''
+    fullBeat = (((CT()-lastBeat)>=(timing-(range/2)+offset)) and ((CT()-lastBeat)<=(timing+(range/2)+offset))) or (((CT()-lastBeat)<=range/2+offset) and ((CT()-lastBeat)>=0))
+    return fullBeat
 
 #Extras 
 currentInput=0
+
+ResetInputs()
 #run time
 while(True):
 
-    timer = Timer2(rythem[0], lastBeat)
-    if(timer):
-        lastBeat = CT()
-#checking inputs
-    NumInputs()
-    currentInput=0
-    for input in inputs:
-        currentInput += 1
+    #while(inMenu)
 
-        if input:
-            if alreadyTrackedInput[currentInput]==False:
-                success[currentInput] = Timer3(rythem[currentInput],lastbeats[currentInput],timeRange)
-            alreadyTrackedInput[currentInput]=True
+    while(inGame):
+
+
+        if(Timer2(homeRythem, lastBeat)):
+            lastBeat = CT()
+
+
+        #checking inputs
+        GetInputs()
+        currentInput=0
+        for input in inputs:
+            #print(str(currentInput)+"---------")
+
+            if ((successNotReset[currentInput]) and (Timer2(rythem[currentInput],lastBeats[currentInput]-(timeRange/2)))):
+                success[currentInput] = False
+                successNotReset[currentInput] = False
+                false1.value=False
+                correctLED(False,currentInput)
+                print("success: reset")
+            if Timer2(rythem[currentInput],lastBeats[currentInput]):
+                successNotReset[currentInput] = True
+                lastBeats[currentInput] = CT()
+                '''if not success[currentInput]:
+                    strikes += 1'''
+                print("lastBeat: reset, Strikes: "+str(strikes) )
+            
+            if (input):
+                if (not alreadyTrackedInput[currentInput]):
+                    print("(")
+                    alreadyTrackedInput[currentInput]=True
+                    #adds strikes for pressing after a succes and pressing outside of acceptable zone
+                    if (not Timer3(rythem[currentInput],lastBeats[currentInput],timeRange)):
+                        strikes += 1
+                        false1.value=True
+                        print("Strikes   __1__   :"+str(strikes))
+                    elif (success[currentInput]):
+                        strikes += 1
+                        false1.value=True
+                        print("Strikes   __2__   :"+str(strikes))
+                    else:
+                        success[currentInput] = True
+                        score += 1
+                        correctLED(True,currentInput)
+                        print("Score: "+str(score))
+
+            else:   # One registered press per on/off cycle
+                if(alreadyTrackedInput[currentInput]):
+                    print(")")
+                alreadyTrackedInput[currentInput]=False 
+            currentInput += 1
+
+
+        if Timer3Offset(homeRythem,lastBeat,0.1,-timeRange/2):# Updating rythem light
+            ryth1.value = True
         else:
-            alreadyTrackedInput[currentInput]=False
-
-        if Timer2(rythem[currentInput],lastbeats[currentInput]):
-            lastbeats[currentInput] = CT
-    
-    if Timer2(timer,3):# updating rythem light
-        ryth1.value = True
-    time.sleep(0.01)
+            ryth1.value = False
+        if strikes >= 300:
+            inGame = False
+        time.sleep(0.001)
